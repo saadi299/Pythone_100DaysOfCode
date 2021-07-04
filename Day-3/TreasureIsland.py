1aa22df705bf67b5d554dc5e511fe8e6a7608dd2
saadi299 <saadimohammed149@gmail.com>
2021-07-04 13:45:57 +0600

    Treasure Haunting Game by using Control flow and Conditional statements.

---
+++ Day-3/TreasureIsland.py
@@ -0,0 +1,49 @@
+print('''|~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+                   The Treasure Island
+ \_/__________________________________________________________________/''')
+
+print("Welcome to the game "+input("Insert your name \n"));
+direction=input("Let's start your treasure hunting.Choose which Direction you want to go? Left or Right \n").lower();
+
+
+
+if direction=="left":
+    swimOrwait=input("Great!There is a pond in front of you. Would you like to Swim or Wait? \n").lower();
+    if swimOrwait=="wait":
+        door=input("omg! Somehow you manage to survive. Now you came infrot of three doors. It's frigtenning.Choose which door you want to go? Red/Yellow/Blue \n").lower();
+        if door=="yellow":
+            print("Congratulations! You have won the game.")
+        elif door=="red":
+            print("You are burned by fire. Game is over")
+        elif door=="blue":
+            print("You are eaten by beast. Game is Over")
+        else:
+            print("Game Over!")
+    else:
+        print(f"Because you choose {swimOrwait}. Crocodail eatb you. Game Over!")
+else:
+    print(f"Because you choose {direction} .You have fallen into a hole. Game is over!")
+
+
+
+
